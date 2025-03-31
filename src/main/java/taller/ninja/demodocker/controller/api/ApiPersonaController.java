c1f9062764be393e89adc1dd31b629ab90a950db
sebashh26 <sebashh26@gmail.com>
2025-03-31 12:43:26 -0500

    Change the password for the image can up with teh docker compose file

--- src/main/java/taller/ninja/demodocker/controller/api/ApiPersonaController.java
+++ src/main/java/taller/ninja/demodocker/controller/api/ApiPersonaController.java
@@ -1,6 +1,7 @@
 package taller.ninja.demodocker.controller.api;
 
 import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
